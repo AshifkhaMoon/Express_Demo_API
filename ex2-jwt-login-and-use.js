e1d5c5af4ec5e6b0187148a90e70ec2009632298
Moon Ashif <113497765+AshifkhaMoon@users.noreply.github.com>
2024-02-26 11:52:21 +0200

    JWT Auth Exercise

---
+++ ex2-jwt-login-and-use.js
@@ -0,0 +1,7 @@
+/*Exercise 2
+
+Create a new express based server, create one route into it and 
+use JWT security scheme in your route to protect it.
+
+For this to work you need another route which is used to create the JWT
+*/
